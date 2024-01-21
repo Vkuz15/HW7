9384dd72a330e0f87ca985d7a723b6b845d4d296
Владимир <vkuznecov1504@mail.ru>
2024-01-21 12:01:03 +0400

    Объявили переменные total, cash, mouth С помощью цикла while вывели необходимое количество месяцев для накопления нужной суммы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задача 1
+
+        int total = 0;
+        int cash = 15_000;
+        int mouth = 0;
+        while (total <= 2_459_000) {
+            total = total + cash;
+            total = total + total/100;
+            mouth = mouth + 1;
+            System.out.println("Месяц " + mouth + " сумма накоплений равна " + total + " рублей");
+        }
     }
 }
